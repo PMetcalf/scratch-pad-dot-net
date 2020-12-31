8193ff3c16f2cd0f14249608320714cae1017c75
Paul Metcalf <paulmetcalf@hotmail.com>
2020-12-31 11:29:37 +0000

    Work in progress.

--- Python/python_oop.py
+++ Python/python_oop.py
@@ -4,6 +4,47 @@ Python OOP
 Scratch pad for OOP concepts in Python.
 '''
 
+'''
+--Classes--
+'''
+
+class Parrot:
+    
+    # Class attribute
+    species = 'bird'
+
+    # Instance attribute
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # Instance methods
+    def sing(self, song):
+        return "{} sings {}".format(self.name, song)
+
+    def dance(self):
+        return "{} is now dancing".format(self.name)
+
+# Instantiate the parrot class
+blue = Parrot("Blue", 10)
+Woo = Parrot("Woo", 15)
+
+# Access class attributes
+print("Blue is a {}".format(blue.__class__.species))
+print("Woo is also a {}".format(Woo.__class__.species))
+
+# Access the instance attributes
+print("{} is {} years old".format( blue.name, blue.age))
+print("{} is {} years old".format( Woo.name, Woo.age))
+
+# Call instance methods
+print(blue.sing("'Happy'"))
+print(blue.dance())
+
+'''
+--Inheritance--
+'''
+
 # Parent class
 class Bird:
 
@@ -29,47 +70,6 @@ class Penguin(Bird):
 
     def run(self):
         print("Run faster")
-
-class Parrot:
-    
-    # Class attribute
-    species = 'bird'
-
-    # Instance attribute
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    # Instance methods
-    def sing(self, song):
-        return "{} sings {}".format(self.name, song)
-
-    def dance(self):
-        return "{} is now dancing".format(self.name)
-
-'''
---Classes--
-'''
-
-# Instantiate the parrot class
-blue = Parrot("Blue", 10)
-Woo = Parrot("Woo", 15)
-
-# Access class attributes
-print("Blue is a {}".format(blue.__class__.species))
-print("Woo is also a {}".format(Woo.__class__.species))
-
-# Access the instance attributes
-print("{} is {} years old".format( blue.name, blue.age))
-print("{} is {} years old".format( Woo.name, Woo.age))
-
-# Call instance methods
-print(blue.sing("'Happy'"))
-print(blue.dance())
-
-'''
---Inheritance--
-'''
 
 peggy = Penguin()
 peggy.who_is_this()
