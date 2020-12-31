c26a98eaca476b0999b39a648ef4c8cfe17f8420
Paul Metcalf <paulmetcalf@hotmail.com>
2020-12-31 10:46:55 +0000

    Work in progress.

--- Python/python_oop.py
+++ Python/python_oop.py
@@ -14,6 +14,13 @@ class Parrot:
         self.name = name
         self.age = age
 
+    # Instance methods
+    def sing(self, song):
+        return "{} sings {}".format(self.name, song)
+
+    def dance(self):
+        return "{} is now dancing".format(self.name)
+
 # Instantiate the parrot class
 blue = Parrot("Blue", 10)
 Woo = Parrot("Woo", 15)
@@ -25,3 +32,7 @@ print("Woo is also a {}".format(Woo.__class__.species))
 # Access the instance attributes
 print("{} is {} years old".format( blue.name, blue.age))
 print("{} is {} years old".format( Woo.name, Woo.age))
+
+# Call instance methods
+print(blue.sing("'Happy'"))
+print(blue.dance())
