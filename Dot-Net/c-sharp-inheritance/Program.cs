0d8dece32ae3ebc16ea901f82c3dac9dba774352
Paul Metcalf <paulmetcalf@hotmail.com>
2021-01-01 13:23:57 +0000

    Work in progress.

--- Dot-Net/c-sharp-inheritance/Program.cs
+++ Dot-Net/c-sharp-inheritance/Program.cs
@@ -2,6 +2,30 @@
 
 namespace c_sharp_inheritance
 {
+    // Base class
+    public class User
+    {
+        public string Name;
+
+        private string Location;
+
+        // Base class constructor
+        public User()
+        {
+            Console.WriteLine("Base Class Constructor");
+        }
+
+        // Base class method
+        public void GetUserInfo(string loc)
+        {
+            Location = loc;
+
+            Console.WriteLine("Name: {0}", Name);
+            Console.WriteLine("Location: {0}", Location);
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
