ca53d31228b7784aa039b4e76da12fce4201b49b
Paul Metcalf <paulmetcalf@hotmail.com>
2021-01-01 10:06:25 +0000

    Added c# console app.

---
+++ Dot-Net/c-sharp-classes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_sharp_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
