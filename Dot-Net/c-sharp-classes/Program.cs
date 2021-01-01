7d15bd56179209c20e9bfb748936cbcc2d8c99b2
Paul Metcalf <paulmetcalf@hotmail.com>
2021-01-01 10:15:13 +0000

    Work in progress.

--- Dot-Net/c-sharp-classes/Program.cs
+++ Dot-Net/c-sharp-classes/Program.cs
@@ -12,6 +12,25 @@ namespace c_sharp_classes
 
     public class Users
     {
-        //
+        // Class fields
+        public int id = 0;
+        public string name = string.Empty;
+
+        // Class Constructor Statement
+        public Users()
+        {
+
+        }
+
+        public void GetUserDetails(int userid, string username)
+        {
+            id = userid;
+            username = name;
+            Console.WriteLine("Id: {0}, Name: {1}", id, name);
+        }
+
+        // Class Properties
+        public int Designation { get; set; }
+        public int Location { get; set; }
     }
 }
