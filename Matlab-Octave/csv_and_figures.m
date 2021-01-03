ee3f8b3e8bce25c5f7299765e88fb99ac61cb300
Paul Metcalf <paulmetcalf@hotmail.com>
2021-01-03 15:04:26 +0000

    Work in progress.

--- Matlab-Octave/csv_and_figures.m
+++ Matlab-Octave/csv_and_figures.m
@@ -3,6 +3,11 @@
 % Input variables
 input_file_name = "anscombes-quartet.csv";
 input_file_directory = "D:/Developer Area/scratch-pad/Datasets/";
+delimiter = ",";
+skip_header = 1;
 
 % Strings concatenated using matrix format
-file_string = [input_file_directory input_file_name]
+file_string = [input_file_directory input_file_name];
+
+% Load dataset
+data = dlmread(file_string, delimiter, skip_header, 0)
