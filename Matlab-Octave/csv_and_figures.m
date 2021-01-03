beaaab07b5773a61b5245ebf2ef944114cb72fef
Paul Metcalf <paulmetcalf@hotmail.com>
2021-01-03 14:54:35 +0000

    Work in progress.

---
+++ Matlab-Octave/csv_and_figures.m
@@ -0,0 +1,8 @@
+% Opening and Working with CSV Files
+
+% Input variables
+input_file_name = "anscombes-quartet.csv";
+input_file_directory = "D:/Developer Area/scratch-pad/Datasets/";
+
+% Strings concatenated using matrix format
+file_string = [input_file_directory input_file_name]
