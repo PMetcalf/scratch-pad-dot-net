9552bfc9c1e195253aa5fafcc6c5bc7c72270402
PMetcalf <paulmetcalf@hotmail.com>
2021-01-06 22:13:01 +0000

    Work in progress.

--- Python/Web Scraping/python_webscraping_beautifulsoup.py
+++ Python/Web Scraping/python_webscraping_beautifulsoup.py
@@ -19,7 +19,6 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 # Find Elements of interest
 results = soup.find(id = 'ResultsContainer')
-print(results.prettify())
 
 # Find job elements
 job_elems = results.find_all('section', class_ = 'card-content')
@@ -28,10 +27,14 @@ job_elems = results.find_all('section', class_ = 'card-content')
 for job_elem in job_elems:
     # Each job_elem is a new BeautifulSoup object.
     # You can use the same methods on it as you did before.
-    title_elem = job_elem.find('h2', class_ = 'title')
-    company_elem = job_elem.find('div', class_ = 'company')
-    location_elem = job_elem.find('div', class_ = 'location')
-    print(title_elem)
-    print(company_elem)
-    print(location_elem)
-    print() # Add a space
+    try:
+        title_elem = job_elem.find('h2', class_ = 'title')
+        company_elem = job_elem.find('div', class_ = 'company')
+        location_elem = job_elem.find('div', class_ = 'location')
+        print(title_elem.text)
+        print(company_elem.text)
+        print(location_elem.text)
+        print() # Add a space
+
+    except:
+        pass
