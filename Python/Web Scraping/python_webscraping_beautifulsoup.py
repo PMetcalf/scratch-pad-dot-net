10c4d63b92327b9514644b973d04092ec57711b4
PMetcalf <paulmetcalf@hotmail.com>
2021-01-06 16:51:08 +0000

    Work in progress.

--- Python/Web Scraping/python_webscraping_beautifulsoup.py
+++ Python/Web Scraping/python_webscraping_beautifulsoup.py
@@ -9,10 +9,14 @@ from bs4 import BeautifulSoup
 import requests
 
 # Constants
-URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Liverpool'
+URL = 'https://www.monster.co.uk/jobs/search/?q=Python-Developer&where=Liverpool__2C-NW'
 
 # Retrieve page
 page = requests.get(URL)
 
 # Start parsing
 soup = BeautifulSoup(page.content, 'html.parser')
+
+# Find Elements of interest
+results = soup.find(id = 'ResultsContainer')
+print(results.prettify())
