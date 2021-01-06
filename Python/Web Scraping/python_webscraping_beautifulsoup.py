d08c4b7b325a1ea1ff85f9bdc62992c6c8fac8c5
PMetcalf <paulmetcalf@hotmail.com>
2021-01-06 16:41:00 +0000

    Work in progress.

--- Python/Web Scraping/python_webscraping_beautifulsoup.py
+++ Python/Web Scraping/python_webscraping_beautifulsoup.py
@@ -5,6 +5,7 @@ Webscraping sandbox with Beautiful Soup and requests modules.
 '''
 
 # Module Imports
+from bs4 import BeautifulSoup
 import requests
 
 # Constants
@@ -12,3 +13,6 @@ URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Liverpool
 
 # Retrieve page
 page = requests.get(URL)
+
+# Start parsing
+soup = BeautifulSoup(page.content, 'html.parser')
