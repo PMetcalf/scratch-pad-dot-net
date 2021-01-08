41b70f6e0c92b313bb2afb99ce2b58fce860a3d9
PMetcalf <paulmetcalf@hotmail.com>
2021-01-08 16:48:08 +0000

    Work in progress.

--- Dot-Net/c-sharp-webscraper/Program.cs
+++ Dot-Net/c-sharp-webscraper/Program.cs
@@ -34,7 +34,13 @@ namespace c_sharp_webscraper
             {
                 if(link.Attributes["href"].Value.Contains(".html"))
                 {
-                    homePageLinks.Add(link.Attributes["href"].Value);
+                    try
+                    {
+                        homePageLinks.Add(link.Attributes["href"].Value);
+                    }
+                    catch
+                    {
+                    }
                 }
             }
 
