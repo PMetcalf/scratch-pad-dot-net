10be6745ed543a52825dd1de137970c1e160ea4c
PMetcalf <paulmetcalf@hotmail.com>
2021-01-08 16:18:31 +0000

    Work in progress.

--- Dot-Net/c-sharp-webscraper/Program.cs
+++ Dot-Net/c-sharp-webscraper/Program.cs
@@ -8,9 +8,20 @@ namespace c_sharp_webscraper
 {
     class Program
     {
+        // ScrapingBrowser mimics real browser
+        static ScrapingBrowser _browser = new ScrapingBrowser();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        // Method returns HTML
+        static HtmlNode GetHtml(string url)
+        {
+            WebPage webPage = _browser.NavigateToPage(new Uri(url));
+
+            return webPage.Html;
         }
     }
 }
