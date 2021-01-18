7f946abb49cff36d9c3f7639833c4cd87f937408
PMetcalf <paulmetcalf@hotmail.com>
2021-01-18 08:55:32 +0000

    Added modules.

--- Dot-Net/c-sharp-webscraper/Program.cs
+++ Dot-Net/c-sharp-webscraper/Program.cs
@@ -1,5 +1,12 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using HtmlAgilityPack;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace c_sharp_webscraper
 {
