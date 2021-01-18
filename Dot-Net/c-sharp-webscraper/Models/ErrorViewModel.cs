5135d69053a59390a45bda83b567d2235b323a50
PMetcalf <paulmetcalf@hotmail.com>
2021-01-18 08:48:49 +0000

    New project added.

---
+++ Dot-Net/c-sharp-webscraper/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace c_sharp_webscraper.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
