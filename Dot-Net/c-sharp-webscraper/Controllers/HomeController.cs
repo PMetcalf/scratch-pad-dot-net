ede3178921960e3f323b10ff368a9ba225359c50
PMetcalf <paulmetcalf@hotmail.com>
2021-01-19 09:13:33 +0000

    Added callUrl routine outline.

--- Dot-Net/c-sharp-webscraper/Controllers/HomeController.cs
+++ Dot-Net/c-sharp-webscraper/Controllers/HomeController.cs
@@ -18,8 +18,20 @@ namespace c_sharp_webscraper.Controllers
             _logger = logger;
         }
 
+        private static async Task<string> CallUrl(string fullUrl)
+        {
+            // Create Http client
+
+            // Force connection to use TLS 1.3 so that HTTPS handshake can complete
+
+            // Clear headers, should you add your own
+
+            // Await the response
+        }
+
         public IActionResult Index()
         {
+            string url = "https://en.wikipedia.org/wiki/List_of_programmers";
             return View();
         }
 
