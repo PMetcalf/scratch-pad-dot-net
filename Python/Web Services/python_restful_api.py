4fc83211a0ac452ed24cf671197bc28e940478af
PMetcalf <paulmetcalf@hotmail.com>
2021-01-22 08:58:29 +0000

    Work in progress.

--- Python/Web Services/python_restful_api.py
+++ Python/Web Services/python_restful_api.py
@@ -5,9 +5,15 @@ RESTful API sandbox with request module.
 '''
 
 # Module Imports
+import json
 import requests
 
 url = "http://api.open-notify.org/astros.json"
+
+def json_print(obj):
+    # Create a formatted string of a json object
+    text = json.dumps(obj, sort_keys = True, indent = 4)
+    print(text)
 
 response = requests.get(url)
 
@@ -15,4 +21,4 @@ response = requests.get(url)
 print(response.status_code)
 
 # Print response content
-print(response.json())
+json_print(response.json())
