79c91a578712ab05112fd8ca2341bb1f43cbe5fb
PMetcalf <paulmetcalf@hotmail.com>
2021-01-22 09:07:47 +0000

    Work in progress.

--- Python/Web Services/python_restful_api.py
+++ Python/Web Services/python_restful_api.py
@@ -8,17 +8,34 @@ RESTful API sandbox with request module.
 import json
 import requests
 
-url = "http://api.open-notify.org/astros.json"
+astros_url = "http://api.open-notify.org/astros.json"
+passover_url = "http://api.open-notify.org/iss-pass.json"
 
 def json_print(obj):
     # Create a formatted string of a json object
     text = json.dumps(obj, sort_keys = True, indent = 4)
     print(text)
 
-response = requests.get(url)
+#--- Simple Response - Astronauts ---
 
-# Print response code
+# Get astros response
+response = requests.get(astros_url)
+
+# Print astros response
 print(response.status_code)
-
-# Print response content
 json_print(response.json())
+
+#--- Paramater Response - Passover ---
+
+# Set latitude and longitude
+lat_long_params = {
+    "lat": 53.3,
+    "lon": 3.08
+}
+
+# Get passover response
+passover_response = requests.get(passover_url, params = lat_long_params)
+
+# Print passover response
+print(passover_response.status_code)
+json_print(passover_response.json())
