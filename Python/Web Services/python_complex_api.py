05831f2e5dc74ad7711cc71ea883ec095aebee7e
PMetcalf <paulmetcalf@hotmail.com>
2021-01-24 12:39:04 +0000

    Work in progress.

--- Python/Web Services/python_complex_api.py
+++ Python/Web Services/python_complex_api.py
@@ -12,18 +12,23 @@ APPLICATION_NAME = "API Sandbox App"
 API_KEY = "ad927174be69fdf7dbe167638cbc1184"
 REGISTERED_TO = "paul_metcalf_uk"
 USER_AGENT = "CloudforestTech"
-last_fm_url = "http://ws.audioscrobbler.com/2.0/"
 
-headers = {
-    'user-agent': USER_AGENT
-}
+def lastfm_get(payload):
 
-payload = {
-    'api_key': API_KEY,
-    'method': 'chart.gettopartists',
-    'format': 'json'
-}
+    # Define headers and url
+    headers = {'user-agent': USER_AGENT}
+    last_fm_url = "http://ws.audioscrobbler.com/2.0/"
 
-response = requests.get(last_fm_url, headers = headers, params = payload)
+    # Add API key and format to payload
+    payload['api_key'] = API_KEY
+    payload['format'] = 'json'
+
+    # Get response
+    response = requests.get(last_fm_url, headers = headers, params = payload)
+    return response
+
+response = lastfm_get({
+    'method': 'chart.gettopartists'
+})
 
 print(response.status_code)
