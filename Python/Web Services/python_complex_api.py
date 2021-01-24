06ccefc9132158bd179dd02e1c77be2846c0d7d6
PMetcalf <paulmetcalf@hotmail.com>
2021-01-24 12:23:43 +0000

    Work in progress.

--- Python/Web Services/python_complex_api.py
+++ Python/Web Services/python_complex_api.py
@@ -3,3 +3,12 @@ Python Complex API
 
 Workiing with a RESTful API with authentication, rate limiting and pagination.
 '''
+
+# Module Importations
+
+
+# Constants
+APPLICATION_NAME = "API Sandbox App"
+API_KEY = "ad927174be69fdf7dbe167638cbc1184"
+SHARED_SECRET = "1ab4b1412fc708c58e7afb0c03956951"
+REGISTERED_TO = "paul_metcalf_uk"
