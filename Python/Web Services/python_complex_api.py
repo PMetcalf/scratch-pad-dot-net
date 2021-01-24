ded29dd8f4607678d7d5fe5f8bfebd9830ddcbed
PMetcalf <paulmetcalf@hotmail.com>
2021-01-24 12:28:44 +0000

    Work in progress.

--- Python/Web Services/python_complex_api.py
+++ Python/Web Services/python_complex_api.py
@@ -4,11 +4,18 @@ Python Complex API
 Workiing with a RESTful API with authentication, rate limiting and pagination.
 '''
 
-# Module Importations
-
+# Module Imports
+import requests
 
 # Constants
 APPLICATION_NAME = "API Sandbox App"
 API_KEY = "ad927174be69fdf7dbe167638cbc1184"
-SHARED_SECRET = "1ab4b1412fc708c58e7afb0c03956951"
 REGISTERED_TO = "paul_metcalf_uk"
+USER_AGENT = "CloudforestTech"
+last_fm_url = "http://my-api-url"
+
+headers = {
+    'user-agent': 'CloudforestTech'
+}
+
+response = requests.get(last_fm_url, headers = headers)
