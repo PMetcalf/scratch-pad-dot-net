3b0fecf986e5d4cf73a14483fef4b76500ac98c1
PMetcalf <paulmetcalf@hotmail.com>
2021-01-25 07:41:42 +0000

    Work in progress.

--- Python/Web Services/python_complex_api.py
+++ Python/Web Services/python_complex_api.py
@@ -9,10 +9,10 @@ import json
 import requests
 
 # Constants
-#APPLICATION_NAME
-#API_KEY
-#REGISTERED_TO
-#USER_AGENT
+APPLICATION_NAME = "API Sandbox App"
+API_KEY = "ad927174be69fdf7dbe167638cbc1184"
+REGISTERED_TO = "paul_metcalf_uk"
+USER_AGENT = "CloudforestTech"
 
 def lastfm_get(payload):
 
