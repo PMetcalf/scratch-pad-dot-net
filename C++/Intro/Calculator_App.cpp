f664b1409a360d5308beb42315acf8c2428354e6
Paul Metcalf <paulmetcalf@hotmail.com>
2021-02-08 09:54:21 +0000

    Work in progress.

--- C++/Intro/Calculator_App.cpp
+++ C++/Intro/Calculator_App.cpp
@@ -1,13 +1,20 @@
 // Calculator example - This file contains the 'main' function - Program exectution begins and ends there.
 
 #include <iostream>
+#include "Calculator.h"
 
 using namespace std;
 
 int main()
 {
+    double x = 0.0;
+    double y = 0.0;
+    double result = 0.0;
+    char oper = '+';
+
     cout << "Calculator Console App" << endl;
     cout << "Please enter the operation to perform. Format a+b | a-b | a*b | a/b"
         << endl;
-    return 0;
+    
+    
 }
