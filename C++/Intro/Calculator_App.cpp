98114fc44b401a567a1fec7af12f965f45b4673d
Paul Metcalf <paulmetcalf@hotmail.com>
2021-02-08 09:44:05 +0000

    Work in progress.

--- C++/Intro/Calculator_App.cpp
+++ C++/Intro/Calculator_App.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Calculator Console App" << endl << endl;
+    cout << "Calculator Console App" << endl;
     cout << "Please enter the operation to perform. Format a+b | a-b | a*b | a/b"
         << endl;
     return 0;
