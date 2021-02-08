dbba0d9c33eda166dfdbfcdf72b1077068251db5
Paul Metcalf <paulmetcalf@hotmail.com>
2021-02-08 09:29:38 +0000

    Work in progress.

---
+++ C++/Intro/Calculator.cpp
@@ -0,0 +1,8 @@
+// Calculator example - This file contains the 'main' function - Program exectution begins and ends there.
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
