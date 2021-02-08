741d30f9326504b1128b6f88d5a0e38e95fb0b23
Paul Metcalf <paulmetcalf@hotmail.com>
2021-02-08 09:50:39 +0000

    Work in progress.

---
+++ C++/Intro/Calculator.cpp
@@ -0,0 +1,16 @@
+#include "Calculator.h"
+
+double Calculator::Calculate(double x, char oper, double y)
+{
+    switch(oper)
+    {
+        case '+':
+            return x + y;
+        case '+':
+            return x + y;
+        case '+':
+            return x + y;
+        case '+':
+            return x + y;
+    }
+}
