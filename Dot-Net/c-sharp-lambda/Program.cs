fa491ad665145873b5481759217bd77e99aad5b4
Paul Metcalf <paulmetcalf@hotmail.com>
2021-02-24 16:33:41 +0000

    Work in progress.

---
+++ Dot-Net/c-sharp-lambda/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_sharp_lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
